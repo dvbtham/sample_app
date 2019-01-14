1d0a4cbbeb06d8973cdb38d0c5e43a41ca1fe79c
thamdavies <thamdv96@gmail.com>
2019-01-14 21:56:55 +0700

    Fix bugs.

--- app/validators/birthday_validator.rb
+++ app/validators/birthday_validator.rb
@@ -1,10 +1,9 @@
 class BirthdayValidator < ActiveModel::EachValidator
   def validate_each record, attribute, value
-    unless valid_age value.to_date
-      record.errors[attribute] << I18n.t("validators.birthday",
-        min_year: Settings.min_birthday.to_date.year,
-        max_year: (Date.today.year - Settings.min_age))
-    end
+    return if valid_age value.to_date
+    record.errors[attribute] << I18n.t("validators.birthday",
+      min_year: Settings.min_birthday.to_date.year,
+      max_year: (Date.today.year - Settings.min_age))
   end
 
   private
